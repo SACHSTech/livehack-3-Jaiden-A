6d02d7a77a3f3a179aa3efa4948c5494a1c35364
Jaiden Adhvaryu <jaiden.adhvaryu23@ycdsbk12.ca>
2021-03-03 15:58:21 +0000

    submission

--- problem1.py
+++ problem1.py
@@ -1,1 +1,40 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem1.py
+Purpose:	a program that asks you to input your wins and losses and puts you in a specific group
 
+Author:	Adhvaryu.J
+
+Created:	03/03/2021
+------------------------------------------------------------------------------
+'''
+
+print("****** Tournament Tracker ******")
+print("")
+
+wins = 0
+losses = 0
+
+# get amount of wins and losses and add them up
+print("Enter the wins (W) and losses (L) for you team: ")
+for i in range(6):
+  games_played = str(input(""))
+  if games_played == "W":
+    wins = wins + 1
+  elif games_played == "L":
+    losses = losses + 1
+  else :
+    print("Please enter the values W or L again")
+
+# output results based off wins and losses
+if wins == 5 or wins == 6:
+  print("Your team is in group 1")
+elif wins == 3 or wins == 4:
+  print("Your team is in group 2")
+elif wins == 1 or wins == 2:
+  print("Your team is in group 3")
+else :
+  print("Your team has been eliminated from the tournament")
+
+
+  
--- problem2.py
+++ problem2.py
@@ -1,1 +1,32 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem2.py
+Purpose:	a program that allows you to enter the distance you drove until you pass 100 km
 
+Author:	Adhvaryu.J
+
+Created:	03/03/2021
+------------------------------------------------------------------------------
+'''
+
+print("******  Welcome to the DoorDash Distance Tracker  ******")
+print("")
+
+print("** Travel Log **")
+total = 0
+days = 0
+
+# get the distance travelled until they reach 100km
+while total <= 100:
+  distance_travelled = int(input("Enter the distance travelled for the day: "))
+
+  # compute the days it took and average km per day
+  total = total + distance_travelled
+  days = days + 1
+  average = total/days 
+
+# output the amount of days it took to drive 100km
+print("")
+print("** Summary **")
+print("It took " + str(days) + " days to surpass 100km driven.")
+print("The average distance driven per day is " + str(average) + "km")
